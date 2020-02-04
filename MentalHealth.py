e6a283cdc1c661da3bfc7a55f6267a2d37ed9e94
Vividha <50369708+V2dha@users.noreply.github.com>
2020-02-04 21:20:32 +0530

    Update MentalHealth.py

--- MentalHealth.py
+++ MentalHealth.py
@@ -1,7 +1,7 @@
-import numpy as np  # linear algebra
-import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd  
 import matplotlib.pyplot as plt
-#import seaborn as sns
+
 
 from scipy import stats
 from scipy.stats import randint
@@ -22,7 +22,7 @@ from sklearn.model_selection import cross_val_score
 
 
 
-train_df = pd.read_csv(r'C:\Users\vividha\Desktop\Datasets\dummy.csv')
+train_df = pd.read_csv(r'Mental Health.csv')
 
 
 
@@ -36,7 +36,7 @@ train_df = train_df.drop(['comments'], axis= 1)
 train_df = train_df.drop(['state'], axis= 1)
 train_df = train_df.drop(['Timestamp'], axis= 1)
 
-train_df.isnull().sum().max() #just checking that there's no missing data missing...
+train_df.isnull().sum().max() 
 
 
 
@@ -44,7 +44,7 @@ defaultInt = 0
 defaultString = 'NaN'
 defaultFloat = 0.0
 
-# Create lists by data tpe
+
 intFeatures = ['Age']
 stringFeatures = ['Gender', 'Country', 'self_employed', 'family_history', 'treatment', 'work_interfere',
                  'no_employees', 'remote_work', 'tech_company', 'anonymity', 'leave', 'mental_health_consequence',
@@ -53,7 +53,7 @@ stringFeatures = ['Gender', 'Country', 'self_employed', 'family_history', 'treat
                  'seek_help']
 floatFeatures = []
 
-# Clean the NaN's
+
 for feature in train_df:
     if feature in intFeatures:
         train_df[feature] = train_df[feature].fillna(defaultInt)
@@ -66,12 +66,11 @@ for feature in train_df:
 
 
 gender = train_df['Gender'].str.lower()
-#print(gender)
 
-#Select unique elements
+
+
 gender = train_df['Gender'].unique()
 
-#Made gender groups
 male_str = ["male", "m", "male-ish", "maile", "mal", "male (cis)", "make", "male ", "man","msle", "mail", "malr","cis man", "Cis Male", "cis male"]
 trans_str = ["trans-female", "something kinda male?", "queer/she/they", "non-binary","nah", "all", "enby", "fluid", "genderqueer", "androgyne", "agender", "male leaning androgynous", "guy (-ish) ^_^", "trans woman", "neuter", "female (trans)", "queer", "ostensibly male, unsure what that really means"]
 female_str = ["cis female", "f", "female", "woman",  "femake", "female ","cis-female/femme", "female (cis)", "femail"]
@@ -87,7 +86,7 @@ for (row, col) in train_df.iterrows():
     if str.lower(col.Gender) in trans_str:
         train_df['Gender'].replace(to_replace=col.Gender, value='trans', inplace=True)
 
-#Get rid of bullshit
+
 stk_list = ['A little about you', 'p']
 train_df = train_df[~train_df['Gender'].isin(stk_list)]
 
@@ -95,7 +94,7 @@ print(train_df['Gender'].unique())
 
 train_df['Age'].fillna(train_df['Age'].median(), inplace = True)
 
-# Fill with media() values < 18 and > 120
+
 s = pd.Series(train_df['Age'])
 s[s<18] = train_df['Age'].median()
 train_df['Age'] = s
@@ -103,7 +102,6 @@ s = pd.Series(train_df['Age'])
 s[s>120] = train_df['Age'].median()
 train_df['Age'] = s
 
-#Ranges of Age
 train_df['age_range'] = pd.cut(train_df['Age'], [0,20,30,65,100], labels=["0-20", "21-30", "31-65", "66-100"], include_lowest=True)
 train_df['self_employed'] = train_df['self_employed'].replace([defaultString], 'No')
 print(train_df['self_employed'].unique())
@@ -117,7 +115,6 @@ for feature in train_df:
     le.fit(train_df[feature])
     le_name_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
     train_df[feature] = le.transform(train_df[feature])
-    # Get labels
     labelKey = 'label_' + feature
     labelValue = [*le_name_mapping]
     labelDict[labelKey] = labelValue
@@ -136,13 +133,12 @@ feature_cols = ['Age', 'Gender', 'family_history', 'benefits', 'care_options', '
 X = train_df[feature_cols]
 y = train_df.treatment
 
-# split X and y into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
 
 logreg = LogisticRegression()
 logreg.fit(X_train, y_train)
 
-# make class predictions for the testing set
+
 y_pred = logreg.predict(X_test)
 y_prob = logreg.predict_proba(X_test)
 
